740d2e5c559e44171b6631026e7c0dd44d62a2cc
koushikmotif2025 <koushikmotif2025@users.noreply.github.com>
2025-02-18 07:38:46 +0000

    deploy: 2b48a213fb3aca5428dffaa99b53687c43f88f5f

--- tests/testdata/user-data.ts
+++
@@ -1,8 +0,0 @@
-import { faker } from '@faker-js/faker';
-
-export function generateRandomUser(): any {
-    return {
-        name: faker.person.fullName(),
-        job: faker.helpers.arrayElement(['Developer', 'Engineer', 'Manager', 'Designer'])
-    };
-}
